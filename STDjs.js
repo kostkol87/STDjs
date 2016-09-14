4a439718d82a4cb43c142808aa54eff9aa860ede
Konstantin Kolesov <Konstantin_Kolesov@epam.com>
2016-09-14 10:22:37 +0300

    added additional params to function, vars were hidden in clojure

--- STDjs.js
+++ STDjs.js
@@ -1,33 +1,39 @@
-var sec = 1000,
-    min = 60 * sec,
-    hour = 60 * min,
-    day = 24 * hour;
+var formatDiff = function (longToFormat, ms, s, m, h, d) {
+    var sec = 1000,
+        min = 60 * sec,
+        hour = 60 * min,
+        day = 24 * hour;
+    
+    var _ms = ms ? ms : 'ms ',
+        _s  = s  ? s  : 's ',
+        _m  = m  ? m  : 'm ',
+        _h  = h  ? h  : 'h ',
+        _d  = d  ? d  : 'd ';
 
-var formatDiff = function (longToFormat) {
     if (longToFormat <= sec) {
-        return longToFormat + "ms ";
+        return longToFormat + _ms;
     }
     if (longToFormat <= min) {
-        return (longToFormat / sec | 0) + "s "
-            + (longToFormat % sec | 0) + "ms ";
+        return (longToFormat / sec | 0) + _s
+            + (longToFormat % sec | 0) + _ms;
     }
     if (longToFormat <= hour) {
-        return (longToFormat / min | 0) + "m "
-            + ((longToFormat % min | 0) / sec | 0) + "s "
-            + ((longToFormat % min) % sec) + "ms ";
+        return (longToFormat / min | 0) + _m
+            + ((longToFormat % min | 0) / sec | 0) + _s
+            + ((longToFormat % min) % sec) + _ms;
     }
     if (longToFormat <= day) {
-        return (longToFormat / hour | 0) + "h "
-            + ((longToFormat % hour | 0) / min | 0) + "m "
-            + (((longToFormat % hour) % min) / sec | 0) + "s "
-            + (((longToFormat % hour) % min) % sec) + "ms ";
+        return (longToFormat / hour | 0) + _h
+            + ((longToFormat % hour | 0) / min | 0) + _m
+            + (((longToFormat % hour) % min) / sec | 0) + _s
+            + (((longToFormat % hour) % min) % sec) + _ms;
     }
     if (longToFormat > day) {
-        return (longToFormat / day | 0) + "d "
-            + ((longToFormat % day | 0) / hour | 0) + "h "
-            + (((longToFormat % day) % hour) / min | 0) + "m "
-            + ((((longToFormat % day) % hour) % min) / sec | 0) + "s "
-            + ((((longToFormat % day) % hour) % min) % sec | 0) + "ms ";
+        return (longToFormat / day | 0) + _d
+            + ((longToFormat % day | 0) / hour | 0) + _h
+            + (((longToFormat % day) % hour) / min | 0) + _m
+            + ((((longToFormat % day) % hour) % min) / sec | 0) + _s
+            + ((((longToFormat % day) % hour) % min) % sec | 0) + _ms;
     }
 };
 
